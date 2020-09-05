582b62cfc9445a5a1574a304f4126f4d0eba2ec8
coder_iota <akshat732000@gmail.com>
2020-09-05 17:46:10 +0530

    Added SignUp route

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require("express");
-const { response } = require("express");
+const bodyParser = require("body-parser");
 
-app = express()
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/public/index.html");
 });
+
+app.get("/signup", function(req, res){
+  res.sendFile(__dirname + "/public/signup.html")
+});
+
+app.post("/signup", function(req, res){
+  var userData = {
+    userName : req.body.userName,
+    password : req.body.password,
+    aadhar : req.body.aadhar,
+    employerName : req.body.employerName,
+    gender : req.body.gender,
+    dob : req.body.dob,
+    mobNumber : req.body.mobNumber,
+  };
+  res.redirect("/");
+})
 
 app.listen(3000, function(){
     console.log("Server Started at port 3000.");
